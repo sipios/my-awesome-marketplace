116cf9a1ca7f6c80c74a9c19642c2660f93ca44b
Shoun ICHIDA <shouni@sipios.com>
2023-02-21 22:33:13 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      
+        <p className="text-3xl font-bold underline">
+          Hello !
+        </p>
+
+    </div>
+  )
+}
+
+export default App
